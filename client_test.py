5990cb0d4785faa3b25b05e0551566bdbad4e4a3
Marian <marian.cucu@gmail.com>
2020-07-26 17:05:21 +0100

    Create Patch File

--- client_test.py
+++ client_test.py
@@ -1,5 +1,5 @@
 import unittest
-from client3 import getDataPoint
+from client3 import *
 
 class ClientTest(unittest.TestCase):
   def test_getDataPoint_calculatePrice(self):
@@ -8,6 +8,8 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+        self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'], (quote['top_bid']['price'] + quote['top_ask']['price'])/2))
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -15,9 +17,32 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+        self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'], (quote['top_bid']['price'] + quote['top_ask']['price'])/2))
+ 
 
 
   """ ------------ Add more unit tests ------------ """
+  
+  #check getDataPoint - if calculated price is between the bid_price and ask_price
+  def test_correct_average(self):
+      quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
+      for quote in quotes:
+          a = getDataPoint(quote)[1]  #bid
+          b = getDataPoint(quote)[2]  #ask
+          c = getDataPoint(quote)[3]  #calculated price
+          self.assertTrue( a < c or b < c)
+      
+  # check getRatio - the case when price_b is zero
+  def test_priceB_zero (self):
+      self.assertIsNone(getRatio (120, 0))
+   
+  #check getRatio - the case when price_a is zero
+  def test_priceA_zero(self):
+      self.assertEqual(getRatio (0, 125), 0)
+        
+
+      
 
 
 
